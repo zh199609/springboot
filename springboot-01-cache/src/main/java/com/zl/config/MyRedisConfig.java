e5af0c34f32cb6f53d61e19e42f193d633e9efcc
zh199609 <1120123073@qq.com>
2018-10-30 22:16:15 +0800

    add->springboot_01

--- springboot-01-cache/src/main/java/com/zl/config/MyRedisConfig.java
+++ springboot-01-cache/src/main/java/com/zl/config/MyRedisConfig.java
@@ -16,25 +16,42 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class MyRedisConfig {
+	/*
+	 * @Bean public RedisTemplate<Object, Object> redisTemplate(
+	 * RedisConnectionFactory redisConnectionFactory) throws UnknownHostException {
+	 * RedisTemplate<Object, Object> template = new RedisTemplate<>();
+	 * template.setConnectionFactory(redisConnectionFactory);
+	 * Jackson2JsonRedisSerializer<Object> serializer = new
+	 * Jackson2JsonRedisSerializer<>(Object.class); //
+	 * 设置值（value）的序列化采用FastJsonRedisSerializer。
+	 * template.setValueSerializer(serializer);
+	 * template.setHashValueSerializer(serializer); //
+	 * 设置键（key）的序列化采用StringRedisSerializer。 template.setKeySerializer(new
+	 * StringRedisSerializer()); template.setHashKeySerializer(new
+	 * StringRedisSerializer()); template.setDefaultSerializer(serializer);
+	 * template.afterPropertiesSet(); System.err.println("序列化完成？？"); return
+	 * template; }
+	 */
+
 	@Bean
-	public RedisTemplate<Object, Object> redisTemplate(
-			RedisConnectionFactory redisConnectionFactory) throws UnknownHostException {
+	public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory)
+			throws UnknownHostException {
 		RedisTemplate<Object, Object> template = new RedisTemplate<>();
 		template.setConnectionFactory(redisConnectionFactory);
 		Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
-		 // 设置值（value）的序列化采用FastJsonRedisSerializer。
-		template.setValueSerializer(serializer);
-		template.setHashValueSerializer(serializer);
-		 // 设置键（key）的序列化采用StringRedisSerializer。
-		template.setKeySerializer(new StringRedisSerializer());
-		template.setHashKeySerializer(new StringRedisSerializer());
+		// 设置值（value）的序列化采用FastJsonRedisSerializer。
+		//template.setValueSerializer(serializer);
+		//template.setHashValueSerializer(serializer);
+		// 设置键（key）的序列化采用StringRedisSerializer。
+		//template.setKeySerializer(new StringRedisSerializer());
+		//template.setHashKeySerializer(new StringRedisSerializer());
 		template.setDefaultSerializer(serializer);
-		template.afterPropertiesSet();
-		System.err.println("序列化完成？？");
 		return template;
 	}
 	
-	
-	
+	/*@Bean
+	public RedisCacheManager cacheManager(RedisTemplate<Object, Object> redisTemplate) {
+		new RedisCacheManager(redisTemplate);
+	}*/
 
 }
