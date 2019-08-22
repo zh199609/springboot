67870ee2378b8a1f528107a28dafc879c39ce9a2
albertzh <albertzh@wicresoft.com>
2019-08-22 19:28:10 +0800

    update

--- springboot_03_logging/src/main/java/com/zl/App.java
+++ springboot_03_logging/src/main/java/com/zl/App.java
@@ -1,11 +1,25 @@
 package com.zl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class App {
+
+    Logger logger = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
         SpringApplication.run(App.class);
     }
+
+    @Bean
+    public TestModel createTestModel() {
+        logger.info("info-createTestModel创建成功");
+        logger.debug("debug-createTestModel创建成功");
+        return new TestModel();
+    }
+
 }
---
+++ springboot_03_logging/src/main/java/com/zl/TestModel.java
@@ -0,0 +1,19 @@
+package com.zl;
+
+/**
+ * @ClassName TestModel
+ * @Description TODO
+ * @Date 2019/8/22 15:44
+ * @Author albertzh
+ **/
+public class TestModel {
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
