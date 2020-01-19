66a0ee47024bb7e9771228df6cb9bdf6ead2f160
albertzh <albertzh@wicresoft.com>
2020-01-19 17:09:46 +0800

    update

--- springboot_api/src/main/java/com/zl/controller/ApiController.java
+++ springboot_api/src/main/java/com/zl/controller/ApiController.java
@@ -5,9 +5,13 @@ import com.zl.config.ApiException;
 import com.zl.config.ApiNotFindException;
 import com.zl.config.NotFindPersonException;
 import com.zl.entity.Person;
+import com.zl.entity.enums.GradeEnum;
+import com.zl.entity.enums.Sex;
 import com.zl.mapper.PersonMapper;
 import com.zl.service.PersonService;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -15,7 +19,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -33,7 +41,7 @@ public class ApiController {
 
     @RequestMapping(value = "add")
     @ResponseBody
-    public Person addEmp(@Valid @RequestBody Person person, BindingResult bindingResult) {
+    public Person addEmp(@Valid @RequestBody Person person, BindingResult bindingResult, HttpServletRequest request) throws IOException {
         if (bindingResult.hasErrors()) {
             List<FieldError> fieldErrors = bindingResult.getFieldErrors();
             fieldErrors.forEach(fieldError -> System.out.println(fieldError.getDefaultMessage()));
@@ -45,6 +53,20 @@ public class ApiController {
         } catch (Exception e) {
             throw new ApiException(e);
         }
+        return person;
+    }
+
+
+    @RequestMapping(value = "/testHttp")
+    @ResponseBody
+    public Person testHttp(HttpServletRequest request) {
+        Person person = new Person();
+        person.setId(1L);
+        person.setName("setName");
+        person.setAge(24);
+        person.setCreateTime(new Date());
+        person.setGrade(GradeEnum.HIGH);
+        person.setSex(Sex.female);
         return person;
     }
 }
