d6bd6d2f87c61b7b653e81fe170a27ae3204e870
albertzh <Password02!>
2020-10-15 11:05:10 +0800

    使用fastjson替换jackson MessageConverter

--- springboot_04_web/src/main/java/com/zl/config/MyMvcConfig.java
+++ springboot_04_web/src/main/java/com/zl/config/MyMvcConfig.java
@@ -1,14 +1,22 @@
 package com.zl.config;
 
-import com.zl.component.LoginHandlerInterceptor;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class MyMvcConfig implements WebMvcConfigurer {
@@ -52,13 +60,54 @@ public class MyMvcConfig implements WebMvcConfigurer {
 
     /**
      * 嵌入式容器定制器
+     *
      * @return
      */
     //@Bean
-    public ConfigurableServletWebServerFactory configurableServletWebServerFactory(){
-    	TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-    	//tomcat.setPort(8088);
-    	return tomcat;
+    public ConfigurableServletWebServerFactory configurableServletWebServerFactory() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        //tomcat.setPort(8088);
+        return tomcat;
     }
 
+    /**
+     * 使用fastjson代替jackson
+     *
+     * @param converters
+     */
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+ /*
+         先把JackSon的消息转换器删除.
+         备注: (1)源码分析可知，返回json的过程为:
+                   Controller调用结束后返回一个数据对象，for循环遍历conventers，找到支持application/json的HttpMessageConverter，然后将返回的数据序列化成json。
+                    具体参考org.springframework.web.servlet.mvc.method.annotation.AbstractMessageConverterMethodProcessor的writeWithMessageConverters方法
+               (2)由于是list结构，我们添加的fastjson在最后。因此必须要将jackson的转换器删除，不然会先匹配上jackson，导致没使用fastjson
+
+        */
+        for (int i = 0; i < converters.size(); i++) {
+            if (converters.get(i) instanceof MappingJackson2HttpMessageConverter) {
+                converters.remove(i);
+            }
+        }
+        FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
+        //自定义fastjson配置
+        FastJsonConfig config = new FastJsonConfig();
+        config.setSerializerFeatures(
+                SerializerFeature.WriteMapNullValue, // 是否输出值为null的字段,默认为false,我们将它打开
+                SerializerFeature.WriteNullListAsEmpty,     // 将Collection类型字段的字段空值输出为[]
+                SerializerFeature.WriteNullStringAsEmpty,   // 将字符串类型字段的空值输出为空字符串
+                SerializerFeature.WriteNullNumberAsZero,    // 将数值类型字段的空值输出为0
+                SerializerFeature.WriteDateUseDateFormat,
+                SerializerFeature.DisableCircularReferenceDetect    // 禁用循环引用
+                );
+        fastJsonHttpMessageConverter.setFastJsonConfig(config);
+        // 添加支持的MediaTypes;不添加时默认为*/*,也就是默认支持全部
+        // 但是MappingJackson2HttpMessageConverter里面支持的MediaTypes为application/json
+        // 参考它的做法, fastjson也只添加application/json的MediaType
+        List<MediaType> fastMediaTypes = new ArrayList<>();
+        fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        fastJsonHttpMessageConverter.setSupportedMediaTypes(fastMediaTypes);
+        converters.add(fastJsonHttpMessageConverter);
+    }
 }
